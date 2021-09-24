ffdf1b843b2b4ae4b4aa4118e25a16e2fd319dd7
jiyeme <jiyecafe@gmail.com>
2021-09-24 10:19:27 +0800

    优化： 删除日志打印代码

--- src/utils/rsa/index.js
+++ src/utils/rsa/index.js
@@ -3,14 +3,9 @@ const RSA = require('./wx_rsa.js')
 const log = require('../log.js')
 
 const RSAEncrypt = (unencrypt) => {
-	log.info('开始加密')
 	let publicKey = RSAPubKey;
-	log.info('publicKey', publicKey)
-	log.info('获取Key')
 	let encrypt_rsa = RSA.KEYUTIL.getKey(publicKey);
-	log.info('开始加密')
 	let encStr = encrypt_rsa.encryptLong(unencrypt);
-	log.info('转base64')
 	let encrypted = RSA.hex2b64(encStr);
 	return encrypted;
 }
